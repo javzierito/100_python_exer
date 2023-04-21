0c6428c0be8a769938bf58c9cc5d006dc8f415c8
Javier Vazquez <javze@vestas.com>
2023-04-22 00:50:17 +0100

    from 17 to 20

---
+++ responses/ex_18.py
@@ -0,0 +1,3 @@
+# what is happening below is that is not possible to acccess the values of the dictionary, intstead it should be accessing the Surname, and Smith will be print
+d = {"Name": "John", "Surname": "Smith"}
+print(d["Surname"])
---
+++ responses/ex_20.py
@@ -0,0 +1,2 @@
+d = {"a": 1, "b": 2, "c": 3}
+suma = sum([j for i,j in d.items()])
