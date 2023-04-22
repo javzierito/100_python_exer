781b01a37e9db204232bc8570e19fee68955aae1
Javier Vazquez <javze@vestas.com>
2023-04-22 23:25:27 +0100

    modified ones

---
+++ responses/ex_56.py
@@ -0,0 +1,9 @@
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+import json
+
+with open("responses/file.json", "w") as json_file:
+    json.dump(d, json_file, indent=4)
